259297423ae6af4e1b1cbb668beb7238aa72f073
Anbu Mani <anbumani91@hotmail.com>
2018-03-31 21:37:28 +0530

    HeaderTemplete
    Header And Footer Templete

---
+++ ListViewHeaderTem/ListViewHeaderTem/ListViewHeaderTem/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewHeaderTem
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MainListView.ItemsSource = new List<string>
+            {
+               "Anbu","Guna","Hari","Mani","Karthi","Sathis","Poovarasan","Mukesh","Karthik"
+                 
+            };
+
+            MainListView.ItemSelected += MainListView_ItemSelected;
+        }
+        private void MainListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            DisplayAlert("This is " + e.SelectedItem.ToString(), "", "Ok");
+        }
+    }
+}
